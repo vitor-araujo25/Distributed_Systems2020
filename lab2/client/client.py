3f77cdd13e8eeb6fd7c39bbc88092ec0a1ca024c
Vitor Araujo <vitorofa@gmail.com>
2020-09-07 23:58:30 -0300

    first client version

--- lab2/client/client.py
+++ lab2/client/client.py
@@ -1,5 +1,6 @@
 import ui
 import sys
+import socket
 
 HOST = "127.0.0.1"
 PORT = 9000
@@ -8,7 +9,27 @@ def main():
     # if len(sys.argv) != 3:
     #     usage
     addr_tuple = (HOST, PORT)
-    ui.start(addr_tuple)
+    start(addr_tuple)
+
+def start(remote_addr):
+    sock = socket.socket(
+        socket.AF_INET,
+        socket.SOCK_STREAM)
+
+    with sock as s:
+        try:
+            s.connect(remote_addr)
+            conn = s.getpeername()
+            print(f"Connected to {conn[0]}:{conn[1]} successfully.")
+            ui.interaction_loop(s)
+
+        except (KeyboardInterrupt, SystemExit):
+            pass
+        except Exception as e:
+            print(f"Uncaught exception raised inside UI loop: {e.message}")
+        finally:
+            print("\nEnding connection...")
+            sock.sendall(b"CLOSE")
 
 if __name__ == "__main__":
     main()
--- lab2/client/ui.py
+++ lab2/client/ui.py
@@ -1,24 +1,53 @@
 import socket
+import json
 
-def start(remote_addr):
-    sock = socket.socket(
-        socket.AF_INET,
-        socket.SOCK_STREAM)
+FULL_WORD_DICT = {}
+ALL_MESSAGES_COLLECTED = False
 
-    with sock as s:
-        try:
-            s.connect(remote_addr)
-            conn = s.getpeername()
-            print(f"Connected to {conn[0]}:{conn[1]} successfully.")
-            while True: 
-                msg = input("Type the file name: ")
-                if msg == "":
-                    print("\nEnding connection...")
-                    s.sendall(b"CLOSE")
-                    break
-                s.sendall(msg.encode())
-                echo = s.recv(1024)
-                print(f"Answer: {echo.decode()}")
-        except (KeyboardInterrupt, SystemExit):
-            print("\nEncerrando conexão...")
-            s.sendall(b"CLOSE")
+def interaction_loop(sock):
+    restore_vars()
+
+    while True:
+        file_name = input("Type the file name: ")
+        if file_name == "":
+            break
+        sock.sendall(file_name.encode())
+
+        while not ALL_MESSAGES_COLLECTED:
+            response_json = sock.recv(2048)
+            try:
+                collect(response_json)
+            except:
+                print(f"An error occurred when decoding message from server. Try again.")
+                break           
+        else:       
+            filtered_word_list = process_full_word_dict()
+            render(filtered_word_list, file_name)
+        restore_vars()
+        
+def restore_vars():
+    FULL_WORD_DICT = {}
+    ALL_MESSAGES_COLLECTED = False
+
+def collect(word_list):
+    partial_word_dict = json.loads(word_list)
+    FULL_WORD_DICT.update(partial_word_dict['words'])
+    
+    #when more_items field has value 'false', no more messages will be sent and
+    #FULL_WORD_DICT is ready to be filtered and ordered
+    if not partial_word_dict['more_items']:
+        ALL_MESSAGES_COLLECTED = True
+
+def process_full_word_dict():
+    words = FULL_WORD_DICT.items()
+    words.sort(key=lambda tup: tup[1], reverse=True)
+    return {word: count for word, count in words[:10]}
+
+def render(word_count, file_name):
+    print()
+    print(f"Most frequent words in file '{file_name}':")
+    print("----------------------------------------------")
+    for word, count in word_count:
+        print(f"  '{word}' --> {count} occurences")
+    print("----------------------------------------------")
+    print()
