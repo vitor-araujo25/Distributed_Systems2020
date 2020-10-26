85caccefad49ada55029df11f65c909b3ce08552
Vitor Araujo <vitorofa@gmail.com>
2020-10-26 00:58:08 -0300

    implementing stabilization primitives

--- lab5/orchestrator.py
+++ lab5/orchestrator.py
@@ -1,10 +1,10 @@
 import multiprocessing as mp
-import hashlib, sys, rpyc, socket
+import hashlib, sys, rpyc, socket, time, threading
 from rpyc.utils.server import ThreadedServer
 from typing import Tuple
 
+
 running_nodes = {}
-HASH_SIZE = 160
 
 def hash(key: str):
     return int(hashlib.sha1(str(key).encode()).hexdigest(), 16)
@@ -38,40 +38,50 @@ def start_nodes(n: int):
 
     
     # addr, finger_table = list(initial_finger_tables.items())[0]
-    
-    p = mp.Process(target=chord_node_main, args=(addr, ))
+
+    bootstrap_addr = free_ports[0]
+
+    p = mp.Process(target=chord_node_main, args=(bootstrap_addr, ))
     p.start()
-    running_nodes[addr] = p
+    running_nodes[bootstrap_addr] = p
     
-    for addr, finger_table in list(initial_finger_tables.items())[1:]:
-        p = mp.Process(target=chord_node_main, args=(addr, finger_table))
+    for addr in free_ports[1:]:
+        time.sleep(0.5)
+        p = mp.Process(target=chord_node_main, args=(addr, bootstrap_addr))
         p.start()
         running_nodes[addr] = p
 
-def chord_node_main(port_no: int, f_table=[]):
-    server = ThreadedServer(ChordNodeService(port=port_no, f_table=f_table), port=port_no)
+def chord_node_main(port_no: int, bootstrap_port_no: int = None):
+    server = ThreadedServer(ChordNodeService(port=port_no, bootstrap_node=bootstrap_port_no), port=port_no)
     server.start()
     
 class ChordNodeService(rpyc.Service):
 
     FT_LENGTH = 20
+    HASH_SIZE = 160
 
-    def __init__(self, port, f_table):
+    def __init__(self, port, bootstrap_node):
         super().__init__()
         #array containing objects of the format (finger, (id_successor, port_successor))
-        self.finger_table = f_table
         self.exposed_id = hash(port)
-        self.port = port
+        self.exposed_port = port
         self.next_finger = 0
-        if len(f_table) == 0:
-            self.exposed_successor = (self.exposed_id, self.port)
-        else:    
-            entry_node = rpyc.connect("localhost", self.finger_table[0][1][1])
-            self.exposed_successor = entry_node.root.find_successor(self.exposed_id)
-        self.exposed_predecessor = None
         
-        threading.Thread(target=housekeeping).start()
-        print(f"{self.port} running!")
+        #creating chord ring from scratch
+        if bootstrap_node is None:
+            self.exposed_successor = (self.exposed_id, self.exposed_port)
+        
+        #joining chord ring from a known bootstrap node
+        else:    
+            entry_node = rpyc.connect("localhost", bootstrap_node)
+            self.exposed_successor = entry_node.root.find_successor(self.exposed_id)
+
+        self.exposed_predecessor = None
+        self.finger_table = [None]*self.FT_LENGTH
+        self.finger_table[0] = {"finger": (self.exposed_id+1) % 2**self.HASH_SIZE, "successor": self.exposed_successor}
+
+        threading.Thread(target=self.housekeeping).start()
+        print(f"{self.exposed_port} running!")
 
         # self.expand_finger_table()
 
@@ -84,13 +94,30 @@ class ChordNodeService(rpyc.Service):
     def housekeeping(self):
         while True:
             time.sleep(0.5)
-            # self.fix_fingers()
-            # self.stabilize()
+            self.fix_fingers()
+            self.stabilize()
 
     def stabilize(self):
-        pass
-        # conn = rpyc.connect("localhost", self.exposed_successor)
-        # x = 
+        conn = rpyc.connect("localhost", self.exposed_successor[1])
+        x: Tuple[int, int] = conn.root.predecessor
+        if x is not None and (x[0] > self.exposed_id or x[0] < self.exposed_successor[0]):
+            self.exposed_successor = x
+        conn.root.notify((self.exposed_id, self.exposed_port))
+
+    def fix_fingers(self):
+        self.next_finger += 1
+        if self.next_finger >= self.FT_LENGTH:
+            self.next_finger = 0
+        finger = (self.exposed_id + 2**self.next_finger) % 2**self.HASH_SIZE
+        print(f"next finger: {self.next_finger}")
+        self.finger_table[self.next_finger] = {
+            "finger": finger, 
+            "successor": self.exposed_find_successor(finger)
+        }
+    
+    def exposed_notify(self, node: Tuple[int, int]):
+        if self.exposed_predecessor is None or (node[0] > self.exposed_predecessor[0] or node[0] < self.exposed_id):
+            self.exposed_predecessor = node
 
     # def expand_finger_table(self):
     #     peer = rpyc.connect("localhost", self.exposed_successor[1])
@@ -101,13 +128,16 @@ class ChordNodeService(rpyc.Service):
     #     self.stable = True
 
     def closest_preceding_node(self, id: int) -> Tuple[int, int]:
-        for i in range(len(self.finger_table)-1, -1, -1):
-            if self.finger_table[i][0] > n and self.finger_table[i][0] < id:
-                return self.finger_table[i][1]
-        return (self.exposed_id, self.port)
+        
+        for i in range(len(self.finger_table) - 1, -1, -1):
+            if self.finger_table[i]["finger"] > n or self.finger_table[i]["finger"] < id:
+                return self.finger_table[i]["successor"]
+        
+        return (self.exposed_id, self.exposed_port)
 
     def exposed_find_successor(self, id: int) -> Tuple[int, int]:
-        if id > self.exposed_id and id <= self.exposed_successor[0]:
+        print(f"id: {id}, n: {self.exposed_id}, succ: {self.exposed_successor}")
+        if id > self.exposed_id or id <= self.exposed_successor[0]:
             return self.exposed_successor
         else:
             pred = self.closest_preceding_node(id)
